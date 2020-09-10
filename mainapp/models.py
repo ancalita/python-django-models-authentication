059dbdad67d5cbeb115c8d6339e64029a9294cce
ancalita <27920906+ancalita@users.noreply.github.com>
2020-09-10 17:22:55 +0100

    completed module 4

--- mainapp/models.py
+++ mainapp/models.py
@@ -5,12 +5,25 @@ from django.urls import reverse
 class BlogPost(models.Model):
     title = models.CharField(max_length=200, unique=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, related_name="posts", on_delete=models.CASCADE)
-    body = models.TextField()
+    body = models.TextField(default='')
     postdate = models.DateTimeField(auto_now_add=True, blank=True)
+    tags = models.ManyToManyField('Tag', related_name='posts')
 
     def __str__(self):
         return self.title
 
     def get_absolute_url(self):
         return reverse('post', args=[str(self.id)])
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+
+    def clean(self):
+        self.name = self.name.lower()
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('tag_posts', args=[str(self.name)])
 
